78defba63f1a7da575f7a5e6df8d50178b9bf1e2
valesg <gvales@metcove.com>
2019-10-05 17:00:51 -0400

    Initial Commit

---
+++ AnyPA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnyPA
+//
+//  Created by Guy Vales on 2019-10-05.
+//  Copyright © 2019 Guy Vales. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
